ef9521935bafeefbc9a557c34531ef66fe75b4db
TariqAHassan <tariqaahassan@gmail.com>
2017-01-02 12:39:44 -0400

    Add Open-i Parameters

---
+++ biovida/images/openi_parameters.py
@@ -0,0 +1,20 @@
+"""
+
+    Dictionaries of Open-i Params
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+"""
+
+openi_image_type_dict = {
+    "c": "CT",
+    "g": "graphic",
+    "m": "MRI",
+    "mc": "microscopy",
+    "p": "PET",
+    "ph": "photograph",
+    "u": "ultrasound",
+    "x": "X-ray"
+}
+
+
