a7aecdbb0049812006c5750ccb4f0684ad8a7fa4
TariqAHassan <tariqaahassan@gmail.com>
2017-01-03 10:39:47 -0400

    Add all Parameters for the All Open-i API

--- biovida/images/openi_parameters.py
+++ biovida/images/openi_parameters.py
@@ -6,15 +6,164 @@
 
 """
 
-openi_image_type_dict = {
-    "c": "CT",
-    "g": "graphic",
-    "m": "MRI",
-    "mc": "microscopy",
-    "p": "PET",
-    "ph": "photograph",
-    "u": "ultrasound",
-    "x": "X-ray"
+
+def _parser(s):
+    """
+    
+    Tool to Conver the lists on https://openi.nlm.nih.gov/services.php?it=xg to dicts.
+    
+    :param s: string copied from the URL above
+    :return:
+    :rtype: ``dict``
+    """
+    from biovida.images.openi_support_tools import cln
+
+    # Split the string into keys and values
+    split_string = [i.split('(') for i in cln(s).split(')')]
+
+    # Define a lambda to clean the values
+    lower_join = lambda x: '_'.join(cln(x).split()).strip().lower()
+
+    # generate the dict
+    return {cln(k[1]).strip(): lower_join(k[0]) for k in split_string if set(k) != set([''])}
+
+
+
+openi_video_params = {
+    '0': 'False',
+    '1': 'True'
 }
+
+
+openi_image_type_params = {
+    'c': 'ct',
+    'g': 'graphic',
+    'm': 'mri',
+    'mc': 'microscopy',
+    'p': 'pet',
+    'ph': 'photograph',
+    'u': 'ultrasound',
+    'x': 'x-ray'
+}
+
+
+openi_rankby_params = {
+    'r': 'newest',
+    'o': 'oldest',
+    'd': 'diagnosis',
+    'e': 'etiology',
+    'oc': 'outcome',
+    'pr': 'prevention',
+    'pg': 'prognosis',
+    't': 'treatment'
+}
+
+
+openi_subset_params = {
+    'b': 'basic_science',
+    'c': 'clinical_journals',
+    'e': 'ethics',
+    's': 'systematic_reviews',
+    'x': 'chest_x-rays'
+}
+
+
+openi_collection_params = {
+    'pmc': 'pubmed_central',
+    'iu': 'indiana_u_chest'
+}
+
+
+openi_fields_params = {
+    'a': 'authors',
+    'ab': 'abstracts',
+    'c': 'captions',
+    'm': 'mentions',
+    'msh': 'mesh',
+    't': 'titles'
+}
+
+
+openi_specialties_params = {
+    'b': 'behavioral_sciences',
+    'bc': 'biochemistry',
+    'c': 'cancer',
+    'ca': 'cardiology',
+    'cc': 'critical_care',
+    'd': 'dentistry',
+    'de': 'dermatology',
+    'dt': 'drug_therapy',
+    'e': 'emergency_medicine',
+    'eh': 'environmental_health',
+    'en': 'endocrinology',
+    'f': 'family_practice',
+    'g': 'gastroenterology',
+    'ge': 'genetics',
+    'gr': 'geriatrics',
+    'gy': 'gynecology_&_obstetrics',
+    'h': 'hematology',
+    'i': 'immunology',
+    'id': 'infectious_diseases',
+    'im': 'internal_medicine',
+    'n': 'nephrology',
+    'ne': 'neurology',
+    'nu': 'nursing',
+    'o': 'ophthalmology',
+    'or': 'orthopedics',
+    'ot': 'otolaryngology',
+    'p': 'pediatrics',
+    'pu': 'pulmonary_diseases',
+    'py': 'psychiatry',
+    'r': 'rheumatology',
+    's': 'surgery',
+    't': 'toxicology',
+    'u': 'urology',
+    'v': 'vascular_diseases',
+    'vi': 'virology'
+}
+
+
+openi_api_search_params = {
+    'video': ('&vid', openi_video_params),
+    'image_type' : ('&it', openi_image_type_params),
+    'rankby' : ('&favor', openi_rankby_params),
+    'subset' : ('&sub', openi_subset_params),
+    'collection' : ('&coll', openi_collection_params),
+    'fields' : ('&fields', openi_fields_params),
+    'specialties' : ('&sp', openi_specialties_params),
+}
+
+
+
+def openi_search_information():
+    """
+
+    Returns a dictionary of the form:
+        search_category: (URL_Parameter, {search term: URL_Parameter})
+
+    :return: search information for the Open-i API.
+    :rtype: ``dict``
+    """
+    # Define a lambda to reverse a dict
+    dict_reverse = lambda d: {v: k for k, v in d.items()}
+
+    # Return the openi_api_search_params dict with the dicts nested therein reversed.
+    return {k: (v[0], dict_reverse(v[1])) for k, v in openi_api_search_params.items()}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
