0c2d529eeb11dedad6e4db88b002d0fafd682726
TariqAHassan <tariqaahassan@gmail.com>
2017-01-03 10:40:51 -0400

    Add Image Processing Module

---
+++ biovida/images/image_processing.py
@@ -0,0 +1,107 @@
+"""
+
+    Tools to Process Images for Machine Learning
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+"""
+
+# Goals:
+#     Drop:
+#       - grids (perhaps split instead?)*
+#       - images with color unless they're photographs
+#
+#     Clean:
+#       - MedPix(R) logo
+#       - all other text
+#
+# *will require a neural net.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
