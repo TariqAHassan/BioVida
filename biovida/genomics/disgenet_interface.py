7b6b898b1db97212597127045b05f024adbe7d5c
TariqAHassan <tariqaahassan@gmail.com>
2017-01-04 15:28:25 -0400

    Create DisgenetInterface Class

--- biovida/genomics/disgenet_interface.py
+++ biovida/genomics/disgenet_interface.py
@@ -9,7 +9,6 @@
 import os
 import requests
 import pandas as pd
-
 from pprint import pprint
 
 # Tool to create required caches
@@ -24,25 +23,25 @@ from biovida.support_tools.support_tools import list_to_bulletpoints
 from biovida.support_tools.printing import dict_pretty_printer
 
 
-# Cache Creation
-cache_path = '/Users/tariq/Google Drive/Programming Projects/BioVida'
-root_gene_path, created_gene_dirs = _package_cache_creator(sub_dir='genomic', cache_path=cache_path, to_create=['disgenet'])
+# ---------------------------------------------------------------------------------------------
+# DisGeNET Reference Data
+# ---------------------------------------------------------------------------------------------
 
 
 _disgenet_delimited_databases = {
     # Source: http://www.disgenet.org/web/DisGeNET/menu/downloads#curated
     # Structure: {database_short_name: {full_name: ..., url: ..., description: ..., number_of_rows_in_header: ...}}
-    'curated': {
-        'full_name': 'Curated Gene-Disease Associations',
-        'url': 'http://www.disgenet.org/ds/DisGeNET/results/curated_gene_disease_associations.tsv.gz',
-        'description': 'The file contains gene-disease associations from UNIPROT, CTD (human subset), ClinVar, Orphanet,'
-                       ' and the GWAS Catalog.',
-        'header': 21
-    },
     'all': {
         'full_name': 'All Gene-Disease Associations',
         'url': 'http://www.disgenet.org/ds/DisGeNET/results/all_gene_disease_associations.tsv.gz',
         'description': 'The file contains all gene-disease associations in DisGeNET.',
+        'header': 21
+    },
+    'curated': {
+        'full_name': 'Curated Gene-Disease Associations',
+        'url': 'http://www.disgenet.org/ds/DisGeNET/results/curated_gene_disease_associations.tsv.gz',
+        'description': 'The file contains gene-disease associations\nfrom UNIPROT, CTD (human subset), ClinVar, Orphanet,'
+                       ' and the GWAS Catalog.',
         'header': 21
     },
     'snp_disgenet': {
@@ -54,11 +53,20 @@ _disgenet_delimited_databases = {
 }
 
 
-def _disgenet_readme():
+# ---------------------------------------------------------------------------------------------
+# Tools for Harvesting DisGeNET Data
+# ---------------------------------------------------------------------------------------------
+
+
+def _disgenet_readme(created_gene_dirs):
     """
+
     Writes the DisGeNET README to disk.
+
+    :param created_gene_dirs:
+    :type created_gene_dirs: ``dict``
     """
-    # URL to readme
+    # URL to DisGeNET README
     readme_url = 'http://www.disgenet.org/ds/DisGeNET/results/readme.txt'
 
     # Resquest
@@ -68,99 +76,132 @@ def _disgenet_readme():
     save_address = os.path.join(created_gene_dirs['disgenet'], 'DisGeNET_README.txt')
 
     # Write if the file does not exist
-    if not os.path.isdir(save_address):
+    if not os.path.isfile(save_address):
+        header("The DisGeNET README has been downloaded to:\n\n {0}\n\n"
+               "Please take the time to review this document.".format(save_address))
         with open(save_address, 'wb') as f:
             f.write(r.content)
 
 
-def _disgenet_delimited_databases_key_error(database):
+class DisgenetInterface(object):
     """
 
-    :param database:
-    :return:
-    """
-    if database not in _disgenet_delimited_databases:
-        raise ValueError("'{0}' is an invalid value for `database`.\n`database` must be one of:\n{1}".format(
-            str(database), list_to_bulletpoints(_disgenet_delimited_databases.keys())))
+    Python Interface for Harvesting Databases from DisGeNET.
 
-
-def disgenet_options(database, pretty_print=False):
-    """
-
-    Options for disgenet Databases.
-    Provides the full name
-
-    :param database: one of 'curated', 'all' or 'snp_disgenet'.
-                     if ``None``, return (or print) a list of databases which can be downloaded.
-                     if a specific database is given, the database's full name and description will be provided.
-    :type database: ``str``
-    :param pretty_print: pretty print the information.
-    :type pretty_print: ``bool``
-    :return: a ``list`` if `database` is ``None``, else ``dict`` with the database's full name and description.
-    :rtype: ``list`` or ``dict``
-    """
-    if database is None:
-        info = list(_disgenet_delimited_databases.keys())
-    elif database in _disgenet_delimited_databases:
-        info = {k: v for k, v in _disgenet_delimited_databases[database].items() if k in ['full_name', 'description']}
-    else:
-        _disgenet_delimited_databases_key_error(database)
-
-    if pretty_print:
-        if database is None:
-            print("Available Databases:\n")
-            print(list_to_bulletpoints(info))
-        else:
-            dict_pretty_printer(info)
-    else:
-        return info
-
-
-def disgenet_database(database='all', verbose=False, download_override=False, snake_case_col_names=False):
-    """
-
-    Tool to download a DisGeNET Database.
-
-    :param database: A database to download. Must be one of: 'curated', 'all' or 'snp_disgenet'.
-                     See ``disgenet_options()`` for more information. Defaults to 'all'.
-    :type database: ``str``
+    :param cache_path: location of the BioVida cache. If one does not exist in this location, one will created.
+                       Default to ``None`` (which will generate a cache in the home folder).
+    :type cache_path: ``str`` or ``None``
     :param verbose: If True, print notice when downloading database.
     :type verbose: ``bool``
-    :param download_override: If True, override any existing database currently cached and download a new one.
-                              Defaults to False.
-    :type download_override: ``bool``
-    :param snake_case_col_names: if True, convert column names to 'snake case' (e.g., 'this_is_snake_case').
-                                 Defaults to False (which will leave the column names in `camelCase`).
-    :type snake_case_col_names: ``bool``
-    :return: a DisGeNET database
-    :rtype: ``Pandas DataFrame``
     """
-    _disgenet_delimited_databases_key_error(database)
 
-    # Download Location
-    db_url = _disgenet_delimited_databases[database]['url']
+    def __init__(self, cache_path=None, verbose=False):
+        """
 
-    # Save Name
-    save_name = "{0}.csv".format(db_url.split("/")[-1].split(".")[0])
+        Initialize the ``DisgenetInterface()`` Class.
 
-    # Save address
-    save_address = os.path.join(created_gene_dirs['disgenet'], save_name)
+        """
+        self._verbose = verbose
 
-    # Download or simply load from cache
-    if download_override or not os.path.isfile(save_address):
-        if verbose:
-            header("Downloading DisGeNET Database... ", flank=False)
-        # Harvest and Save
-        df = pd.read_csv(db_url, sep='\t', header=_disgenet_delimited_databases[database]['header'], compression='gzip')
-        df.to_csv(save_address, index=False)
-    else:
-        df = pd.read_csv(save_address)
+        # Cache Creation
+        ppc = _package_cache_creator(sub_dir='genomic', cache_path=cache_path, to_create=['disgenet'])
+        self._root_gene_path, self._created_gene_dirs = ppc
 
-    #  Rename Columns, if requested
-    if snake_case_col_names:
-        df.columns = list(map(camel_to_snake_case, df.columns))
+        # Check if a readme exists.
+        _disgenet_readme(self._created_gene_dirs)
 
-    return df
+        # Container for the most recently requested database.
+        self.current_database = None
+
+    def _disgenet_delimited_databases_key_error(self, database):
+        """
+
+        :param database:
+        :return:
+        """
+        if database not in _disgenet_delimited_databases:
+            raise ValueError("'{0}' is an invalid value for `database`.\n`database` must be one of:\n{1}".format(
+                str(database), list_to_bulletpoints(_disgenet_delimited_databases.keys())))
+
+    def disgenet_options(self, database, pretty_print=False):
+        """
+
+        Disgenet databases which can be downloaded
+        as well as additional information about the databases.
+
+        :param database: A database to review. Must be one of: 'all', 'curated', 'snp_disgenet' or ``None``.
+                         If a specific database is given, the database's full name and description will be provided.
+                         If ``None``, a list of databases which can be downloaded will be returned (or printed) .
+        :type database: ``str``
+        :param pretty_print: pretty print the information.
+        :type pretty_print: ``bool``
+        :return: a ``list`` if `database` is ``None``, else ``dict`` with the database's full name and description.
+        :rtype: ``list`` or ``dict``
+        """
+        if database is None:
+            info = list(_disgenet_delimited_databases.keys())
+        elif database in _disgenet_delimited_databases:
+            info = {k: v for k, v in _disgenet_delimited_databases[database].items() if k in ['full_name', 'description']}
+        else:
+            self._disgenet_delimited_databases_key_error(database)
+
+        if pretty_print:
+            if database is None:
+                print("Available Databases:\n")
+                print(list_to_bulletpoints(info))
+            else:
+                dict_pretty_printer(info)
+        else:
+            return info
+
+    def disgenet_database(self, database, download_override=False, snake_case_col_names=False):
+        """
+
+        Tool to download a DisGeNET Database.
+
+
+        :param database: A database to download. Must be one of: 'all', 'curated', 'snp_disgenet' or ``None``.
+                         See ``disgenet_options()`` for more information.
+        :type database: ``str``
+        :param download_override: If True, override any existing database currently cached and download a new one.
+                                  Defaults to False.
+        :type download_override: ``bool``
+        :param snake_case_col_names: if True, convert column names to 'snake case' (e.g., 'this_is_snake_case').
+                                     Defaults to False (which will leave the column names in `camelCase`).
+        :type snake_case_col_names: ``bool``
+        :return: a DisGeNET database
+        :rtype: ``Pandas DataFrame``
+        """
+        self._disgenet_delimited_databases_key_error(database)
+
+        # Download Location
+        db_url = _disgenet_delimited_databases[database]['url']
+
+        # Save Name
+        save_name = "{0}.csv".format(db_url.split("/")[-1].split(".")[0])
+
+        # Save address
+        save_address = os.path.join(self._created_gene_dirs['disgenet'], save_name)
+
+        # Download or simply load from cache
+        if download_override or not os.path.isfile(save_address):
+            if self._verbose:
+                header("Downloading DisGeNET Database... ", flank=False)
+            # Harvest and Save
+            df = pd.read_csv(db_url, sep='\t', header=_disgenet_delimited_databases[database]['header'], compression='gzip')
+            df.to_csv(save_address, index=False)
+        else:
+            df = pd.read_csv(save_address)
+
+        #  Rename Columns, if requested
+        if snake_case_col_names:
+            df.columns = list(map(camel_to_snake_case, df.columns))
+
+        # Cache the database
+        self.current_database = df
+
+        return df
+
 
 
 
