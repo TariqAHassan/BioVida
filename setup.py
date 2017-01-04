720d3ecff4f93af5fa62e76eb01c21ba91b26547
TariqAHassan <tariqaahassan@gmail.com>
2017-01-03 22:37:32 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except:
+        return 'Please see: https://github.com/TariqAHassan/BioVida.'
+
+
+setup(
+    name='biovida',
+    version='0.1',
+    author='Tariq A. Hassan',
+    author_email='laterallattice@gmail.com',
+    description=('Automated BioMedical Information Harvesting for Machine Learning Applications.'),
+    long_description=read('docs/README.md'),
+    license='BSD',
+    keywords='medicine, biological sciences, machine learning, data science',
+    url='https://github.com/TariqAHassan/EasyMoney.git',
+    packages=find_packages(),
+    data_files=[('', ['LICENSE.md'])],
+    install_requires=['inflect', 'pandas', 'numpy', 'tqdm'],
+    classifiers=['Development Status :: 2 - Pre-Alpha',
+                 'Natural Language :: English',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'License :: OSI Approved :: BSD License'
+    ],
+    include_package_data=True
+)
