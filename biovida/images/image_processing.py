13fbb7cd35f230e4f9b2aaf2472a2ce69b8ac5d9
TariqAHassan <tariqaahassan@gmail.com>
2017-01-05 03:18:55 -0400

    Add Grayscale Analysis

--- biovida/images/image_processing.py
+++ biovida/images/image_processing.py
@@ -5,56 +5,77 @@
 
 
 """
+# Imports
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image, ImageStat
+
+from biovida.images.openi_interface import OpenInterface
+from biovida.support_tools.printing import pandas_pretty_print
+
 
 # Goals:
 #     Drop:
-#       - grids (perhaps split instead?)*
-#       - images with color unless they're photographs
+#       - grids (perhaps split instead?) *
+#       - images with arrows *
 #
 #     Clean:
-#       - MedPix(R) logo
-#       - all other text
+#       - MedPix(R) logo *
+#       - all other text *
 #
-# *will require a neural net.
+#     Mark:
+#       - whether or an image is grayscale     X
+#
+# Legend:
+#     * = will require a neural net.
+#     p = partially solved
+#     X = Solved
+
+# ------------------------------------------------------------------------------------------
+# Get sample data
+# ------------------------------------------------------------------------------------------
 
 
+# Create an instance of the Open-i data harvesting tool
+opi = OpenInterface(n_bounds_limit=5)
+
+# Get the Data
+opi.search(None, image_type=['mri'])
+df = opi.pull()
+
+# Path to the data
+raw_img_path = opi._created_img_dirs['raw']
 
 
+# ------------------------------------------------------------------------------------------
+# Grayscale Analysis
+# ------------------------------------------------------------------------------------------
 
 
+def grayscale_img(path):
+    """
+
+    Use the PIL library to determine whether or not an image is grayscale.
+    Note: this tool is very conservative. *Any* 'color' will yeild `False`.
+
+    See: http://stackoverflow.com/q/23660929/4898004
+    :param path:
+    :return: ``True`` if grayscale, else ``False``.
+    :rtype: bool
+    """
+    if path is None or pd.isnull(path):
+        return np.NaN
+    stat = ImageStat.Stat(Image.open(path).convert("RGB"))
+    return np.mean(stat.sum) == stat.sum[0]
+
+# Compute whether or not the image is grayscale
+df['grayscale_img'] = df['img_cache_name'].map(lambda i: grayscale_img(os.path.join(raw_img_path, i)))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# ------------------------------------------------------------------------------------------
+# Logo Removal
+# ------------------------------------------------------------------------------------------
 
 
 
