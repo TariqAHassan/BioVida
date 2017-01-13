5ba3d1f06bb07257fe39d2ffc1a5dc83dfee3781
TariqAHassan <tariqaahassan@gmail.com>
2017-01-13 03:49:43 -0400

    Add Logo Analysis

--- biovida/images/image_processing.py
+++ biovida/images/image_processing.py
@@ -14,7 +14,10 @@ from PIL import ImageStat
 from PIL import ImageChops
 from tqdm import tqdm
 
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.preprocessing.image import (ImageDataGenerator,
+                                       array_to_img,
+                                       img_to_array,
+                                       load_img)
 
 # Tools form the image subpackage
 from biovida.images.openi_interface import OpenInterface
@@ -40,10 +43,10 @@ tqdm.pandas("status")
 #   1. Check if grayscale                                                       x
 #         - if false, try to detect a frame
 #               - if frame, crop else ban
-#   2. Look for MedLine(R) logo *
+#   2. Look for MedLine(R) logo
 #         - if true, crop
 #   3. Look for text bar *
-#         - if true, crop
+#         - if true, try crop, else ban
 #   4. Look for arrows or boxes *
 #         - if true, ban
 #   5. Look for image grids *
@@ -111,11 +114,12 @@ opi = OpenInterface(cache_path, download_limit=5500, img_sleep_time=2, records_s
 
 # Get the Data
 opi.search(None, image_type=['mri'])
-df = opi.pull()
+# df = opi.pull()
 
 # Save or Load the database.
 # opi.cache('image_record_database', action='save')
-df = opi.current_search_dataframe
+# df = opi.current_search_dataframe
+# df = opi.image_record_database
 
 # Path to the data
 raw_img_path = opi._created_img_dirs['raw']
@@ -137,7 +141,7 @@ def grayscale_img(img_path):
     :param img_path: path to an image
     :type img_path:``str``
     :return: ``True`` if grayscale, else ``False``.
-    :rtype: bool
+    :rtype: ``bool``
     """
     # See: http://stackoverflow.com/q/23660929/4898004
     if img_path is None or pd.isnull(img_path):
@@ -165,11 +169,11 @@ def require_grayscale_check(x):
     else:
         return x['image_modality_major']
 
+
 # Apply check based on grayscale status. (move lower)
 # df['image_modality_major'] = df.apply(require_grayscale_check, axis=1)
 
 # df[['image_modality_major']][df['img_cache_name'].str.contains("1__PMC4390528_nihms-668294-f0002__L")]
-
 
 # ------------------------------------------------------------------------------------------
 # Run OCR on the images
@@ -182,13 +186,60 @@ def require_grayscale_check(x):
 #
 # print(pytesseract.image_to_string(img))
 
-
 # ------------------------------------------------------------------------------------------
 # Watermark Cleaning
 # ------------------------------------------------------------------------------------------
 
 from biovida.images.models.template_matching import robust_match_template
+from biovida.images.models.temp import resources_path
 
+base_images = [os.path.join(raw_img_path, i) for i in os.listdir(raw_img_path) if i.endswith(".png")]
+
+def logo_analysis(x, threshold=0.25, x_greater_check=1/3.0, y_greater_check=1/2.0):
+    """
+
+    Wraps ``biovida.images.models.template_matching.robust_match_template()``.
+
+    :param x: passed from df.apply().
+    :type x: ``Pandas Series``
+    :param threshold:
+    :type threshold: ``float``
+    :param x_greater_check:
+    :type x_greater_check: ``float``
+    :param y_greater_check:
+    :type y_greater_check: ``float``
+    :return:
+    :rtype: ``tuple``
+    """
+    # if not grayscale, skip.
+
+    if 'medpix' not in x['journal_title'].lower():
+        return np.NaN
+
+    # Pattern
+    medline_template_img = os.path.join(resources_path, "medpix_logo.png")
+
+    # The image to check
+    base_images_p = x['img_cache_name_full']
+
+    # Run the matching algorithm
+    match, base_img_shape = robust_match_template(pattern_img_path=medline_template_img, base_img_path=base_images[0])
+
+    # Check match quality
+    if match['match_quality'] < threshold:
+        return np.NaN
+
+    # Check the box is in the top right
+    box_bottom_left = match['box']['bottom_left']
+
+    if box_bottom_left[0] < (base_img_shape[0] * x_greater_check) or \
+        box_bottom_left[1] > (base_img_shape[1] * y_greater_check):
+        return np.NaN
+    else:
+        return box_bottom_left
+
+
+df['logo_loc'] = df.progress_apply(logo_analysis, axis=1)
 
 
 # ------------------------------------------------------------------------------------------
@@ -198,8 +249,7 @@ from biovida.images.models.template_matching import robust_match_template
 
 # Load the CNN
 ircnn = ImageRecognitionCNN(data_path)
-ircnn.conv_net()
-ircnn.load(os.path.join(data_path, "10_epoch_new_model_2.h5"), override_existing=True)
+ircnn.load(os.path.join(data_path, "10_epoch_new_model_2.h5"), override_existing=True, default_model_load=True)
 
 # Reverse the classes dict
 reversed_data_classes = dict_reverse(ircnn.data_classes)
