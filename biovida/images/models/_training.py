01e1dd402edeb9ff4647abf23d57b7a07f52086b
TariqAHassan <tariqaahassan@gmail.com>
2017-01-18 02:25:31 -0400

    Refactor

--- biovida/images/models/_training.py
+++ biovida/images/models/_training.py
@@ -5,57 +5,38 @@
 
 """
 # General Imports
-from biovida.images.models.temp import data_path
+import numpy as np
+import scipy.misc
+from biovida.images.models.temp import convent_data_path
+from keras.preprocessing.image import load_img
+
+# Import the Image Recognition Convnet
+from biovida.images.models.img_classification import ImageRecognitionCNN
+
 
 # ------------------------------------------------------------------------------------------
 # Image Classification
 # ------------------------------------------------------------------------------------------
 
-# Import the CNN Model
-from biovida.images.models.img_classification import ImageRecognitionCNN
 
-# Define Class and Run
-ircnn = ImageRecognitionCNN(data_path)
-ircnn.conv_net()
+def _image_rcognition_cnn_training(nb_epoch, training_data_path, save_name):
+    """
 
-ircnn.fit(nb_epoch=10)
-ircnn.save("10_epoch_new_model_2")
+    Train the model.
 
+    :param nb_epoch:
+    :param training_data_path:
+    :param save_name:
+    :return:
+    """
 
+    ircnn = ImageRecognitionCNN(training_data_path)
+    ircnn.convnet()
 
+    ircnn.fit(nb_epoch=nb_epoch)
+    ircnn.save(save_name)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# _image_rcognition_cnn_training(nb_epoch, training_data_path, save_name)
 
 
 
