80ed900ee0ba1dd59f914b08d72346c744f994de
TariqAHassan <tariqaahassan@gmail.com>
2017-01-18 02:28:26 -0400

    Refactor Code

--- biovida/images/image_processing.py
+++ biovida/images/image_processing.py
@@ -6,104 +6,78 @@
 """
 # Imports
 import os
-import pytesseract
 import numpy as np
 import pandas as pd
 from PIL import Image
 from PIL import ImageStat
-from PIL import ImageChops
 from tqdm import tqdm
-
-from keras.preprocessing.image import (ImageDataGenerator,
-                                       array_to_img,
-                                       img_to_array,
-                                       load_img)
+from pprint import pprint
 
 # Tools form the image subpackage
-from biovida.images.resources import *
+from biovida.images.resources import * # needed to generate medpix logo. Refactor.
 from biovida.images.models.temp import resources_path
 from biovida.images.openi_interface import OpenInterface
+
+# Models
+from biovida.images.models.border_detection import border_detection
 from biovida.images.models.img_classification import ImageRecognitionCNN
 from biovida.images.models.template_matching import robust_match_template
-
-# General Support Tools
-from biovida.support_tools.support_tools import dict_reverse
-from biovida.support_tools.printing import pandas_pretty_printer
 
 # Tool to create required caches
 from biovida.support_tools._cache_management import _package_cache_creator
 
-# Suppress np scientific notation
-np.set_printoptions(suppress=True)
-
 # Start tqdm
 tqdm.pandas("status")
 
-# ------------------------------------------------------------------------------------------
+# Suppress np scientific notation
+np.set_printoptions(suppress=True)
+
+# ---------------------------------------------------------------------------------------------
 # General procedure (for Ultrasound, X-ray, CT and MRI):
-# ------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 #
 #   1. Check if grayscale                                                       X
 #         - if false, try to detect a frame
 #               - if frame, crop else ban
 #   2. Look for MedLine(R) logo                                                 P
 #         - if true, crop
-#   3. Look for text bar *
+#   3. Look for text bar                                                        P
 #         - if true, try crop, else ban
-#   4. Look for arrows or boxes *
+#   4. Look for border                                                          P
+#         - if true, try crop, else ban
+#   5. Look for arrows or boxes *
 #         - if true, ban
-#   5. Look for image grids *
+#   6. Look for image grids *
 #         - if true, ban
-#   6. Look for graphs *
+#   7. Look for graphs *
 #         - if true, ban
-#   6. Look for faces *
+#   8. Look for faces *
 #         - if true, ban
-#   7. Look for other text in the image *
-#         - if true, ban (or find method to detect text image overlay).
+#   9. Look for other text in the image *
+#         - if true, ban (or find crop).
 #
 # Legend:
 #     * = requires machine learning
 #     p = partially solved
 #     X = solved
 #
-# Notes:
-#     border removal: http://stackoverflow.com/q/10615901/4898004
-#
-# ------------------------------------------------------------------------------------------
-# Temporary
-# ------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+# Temporary Data
+# ---------------------------------------------------------------------------------------------
 
-from biovida.images.models.temp import data_path, cache_path
-
-# Create the required cahces
-pcc = _package_cache_creator(sub_dir='images', cache_path=cache_path, to_create=['intermediate', 'processed', 'raw'])
-root_path, created_img_processing_dirs = pcc
-
-opi = OpenInterface(cache_path)
-df = opi.image_record_database.copy()
+from biovida.images.models.temp import cache_path
 
 # Create an instance of the Open-i data harvesting tool
 opi = OpenInterface(cache_path, download_limit=5500, img_sleep_time=2, records_sleep_main=None)
 
 # Get the Data
 opi.search(None, image_type=['mri'])
-# df = opi.pull()
 
-# Save or Load the database.
-# opi.cache('image_record_database', action='save')
-# df = opi.current_search_dataframe
-# df = opi.image_record_database
+df = opi.image_record_database
 
-# Path to the data
-raw_img_path = opi._created_img_dirs['raw']
-inter_img_path = created_img_processing_dirs['intermediate']
-
-# Create column with values of the form: PATH/image_name
-df['img_cache_name_full'] = df['img_cache_name'].map(lambda i: os.path.join(raw_img_path, i), na_action='ignore')
-
-# ------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 # Grayscale Analysis
-# ------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 
 def grayscale_img(img_path):
     """
@@ -122,7 +96,6 @@ def grayscale_img(img_path):
     stat = ImageStat.Stat(Image.open(img_path).convert("RGB"))
     return np.mean(stat.sum) == stat.sum[0]
 
-
 def require_grayscale_check(x):
     """
 
@@ -140,11 +113,9 @@ def require_grayscale_check(x):
     else:
         return x['image_modality_major']
 
-
-# ------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 # Watermark Cleaning
-# ------------------------------------------------------------------------------------------
-
+# ---------------------------------------------------------------------------------------------
 
 def logo_analysis(x, threshold=0.25, x_greater_check=1/3.0, y_greater_check=1/2.5):
     """
@@ -162,7 +133,7 @@ def logo_analysis(x, threshold=0.25, x_greater_check=1/3.0, y_greater_check=1/2.
     :return:
     :rtype: ``tuple``
     """
-    # if not grayscale, skip.
+    # ToDo: if not grayscale, skip.
 
     if 'medpix' not in x['journal_title'].lower():
         return np.NaN
@@ -189,7 +160,6 @@ def logo_analysis(x, threshold=0.25, x_greater_check=1/3.0, y_greater_check=1/2.
     else:
         return box_bottom_left
 
-
 def _logo_plotting(found_logo_df, start=0, end=10):
     """
     found_logo_df = df[pd.notnull(df['logo_loc'])].copy()
@@ -214,113 +184,57 @@ def _logo_plotting(found_logo_df, start=0, end=10):
         _logo_diving_plotter(imread(k, flatten=True), v[1])
         sleep(1)
 
-# ------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 # Detect Border
-# ------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
 
-
-# ------------------------------------------------------------------------------------------
-# Run OCR on the images
-# ------------------------------------------------------------------------------------------
-
-# Look for 'Uploader'. If not, try to find 'source'.
-
-# img = Image.open(image_path)
-# pytesseract.image_to_string(img)
-# print(pytesseract.image_to_string(img, boxes=True))
-#
-#
-# print(pytesseract.image_to_string(img))
-
-# ------------------------------------------------------------------------------------------
-# Image Classification
-# ------------------------------------------------------------------------------------------
-
-# Load the CNN
-ircnn = ImageRecognitionCNN(data_path)
-ircnn.load(os.path.join(data_path, "10_epoch_new_model_2.h5"), override_existing=True, default_model_load=True)
-
-# Reverse the classes dict
-reversed_data_classes = dict_reverse(ircnn.data_classes)
-
-
-def class_guess(name, img_path, data_classes, img_shape):
+def border_analysis(data_frame
+                     , join=True
+                     , signal_strength_threshold=0.25
+                     , min_border_separation=0.15
+                     , lower_bar_search_space=0.9
+                     , report_signal_strength=False):
     """
 
-    :param img_path:
-    :param data_classes:
+    Wrapper for ``biovida.images.models.border_detection.border_detection()``.
+
+    :param data_frame:
+    :param join: if ``True``, left join with ``data_frame``. Defaults to True.
+    :param signal_strength_threshold:
+    :param min_border_separation:
+    :param lower_bar_search_space:
+    :param report_signal_strength:
     :return:
     """
-    if name is None or pd.isnull(name):
-        return np.NaN
+    def ba_func(image):
+        return border_detection(image,
+                                signal_strength_threshold,
+                                min_border_separation,
+                                lower_bar_search_space,
+                                report_signal_strength)
 
-    # Load the image and rescale
-    img = load_img(os.path.join(img_path, name), target_size=img_shape).convert('RGB')
+    # Run the analysis
+    border_analysis = data_frame['img_cache_name_full'].progress_map(ba_func)
 
-    # Convert to numpy array
-    x = np.asarray(img, dtype='float32')
-    x = x.transpose(2, 0, 1)
-    x = np.expand_dims(x, axis=0)
+    # Convert to a dataframe and return
+    ba_df = pd.DataFrame(border_analysis.tolist()).fillna(np.NaN)
+    return data_frame.join(ba_df, how='left') if join else ba_df
 
-    # ToDo: pass `ircnn.model` in when this is converted to a class.
-    prediction = ircnn.model.predict_classes(x, verbose=0)[0]
-    class_guess = data_classes[prediction]
+borders_and_edges = border_analysis(df, join=True)
 
-    return class_guess
+# ---------------------------------------------------------------------------------------------
+# Image Classification
+# ---------------------------------------------------------------------------------------------
 
-# df = df.drop_duplicates('img_cache_name')
-#
-# # Classify the images
-# df['image_classification'] = df['img_cache_name'].progress_map(
-#     lambda x: class_guess(x, raw_img_path, reversed_data_classes, ircnn.img_shape)
-# )
-#
-#
-# a = df[df['image_classification'].str.contains('grid')]['img_cache_name'].tolist()
-# len(a)
-# a
-#
-# from random import shuffle
-# from time import sleep
-# shuffle(a)
-#
-#
-# for i in a[0:20]:
-#     _img_text(os.path.join(raw_img_path, i))
-#     sleep(2)
+from biovida.images.models.temp import cnn_model_location
 
+# Load the CNN
+ircnn = ImageRecognitionCNN()
+ircnn.load(cnn_model_location, override_existing=True)
 
-
-
-
-
-
-
-
-# ------------------------------------------------------------------------------------------
-# Computations
-# ------------------------------------------------------------------------------------------
-
-
-
-# Compute whether or not the image is grayscale
-# df['grayscale_img'] = df['img_cache_name_full'].progress_map(lambda i: grayscale_img(i))
-
-# Apply check based on grayscale status. (move lower)
-# df['image_modality_major'] = df.apply(require_grayscale_check, axis=1)
-
-# df[['image_modality_major']][df['img_cache_name'].str.contains("1__PMC4390528_nihms-668294-f0002__L")]
-
-
-# # Run Analysis
-# df['logo_loc'] = df.progress_apply(logo_analysis, axis=1)
-#
-# # Logo found in 87% of images!
-# df[pd.notnull(df['logo_loc'])]['logo_loc'].shape[0] / df[df['journal_title'].str.lower().str.contains('medpix')].shape[0]
-
-
-
-
+# Get classes
+# m = ircnn.predict([])
+# pd.Series(m)
 
 
 
