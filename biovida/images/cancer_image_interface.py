42327034d7ced36e75d7179350e662bcf865dc37
TariqAHassan <tariqaahassan@gmail.com>
2017-02-19 11:26:32 -0400

    Update Docstrings

--- biovida/images/cancer_image_interface.py
+++ biovida/images/cancer_image_interface.py
@@ -1136,10 +1136,10 @@ class CancerImageInterface(object):
 
         >>> CancerImageInterface(YOUR_API_KEY_HERE).search(cancer_type='carcinoma', location=['head'])
         ...
-           collection                   cancer_type                           modalities                 subjects    location
-        0  TCGA-HNSC            Head and Neck Squamous Cell Carcinoma  CT, MR, PT                          164    [Head, Neck]
-        1  QIN-HeadNeck         Head and Neck Carcinomas               PT, CT, SR, SEG, RWV                156    [Head, Neck]
-        2  Head-Neck Cetuximab  Head and Neck Carcinomas               CT, PT, RTSTRUCT, RTPLAN, RTDOSE    111    [Head, Neck]
+           collection                   cancer_type                          modalities         subjects    location
+        0  TCGA-HNSC            Head and Neck Squamous Cell Carcinoma  CT, MR, PT                 164     [Head, Neck]
+        1  QIN-HeadNeck         Head and Neck Carcinomas               PT, CT, SR, SEG, RWV       156     [Head, Neck]
+              ...                          ...                                  ...               ...         ...
 
         """
         # Note the time the search request was made
