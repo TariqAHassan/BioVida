f1aa92ab60ef28fe57b1bac1f88113df4b318fac
TariqAHassan <tariqaahassan@gmail.com>
2017-03-07 09:40:37 -0400

    Refactor

--- biovida/images/__init__.py
+++ biovida/images/__init__.py
@@ -1,5 +1,5 @@
 # Expose key Classes at the Subpackage level
-from biovida.images.cancer_image_interface import CancerImageInterface
 from biovida.images.openi_interface import OpeniInterface
 from biovida.images.image_processing import ImageProcessing
+from biovida.images.cancer_image_interface import CancerImageInterface
 from biovida.images.models.image_classification import ImageRecognitionCNN
