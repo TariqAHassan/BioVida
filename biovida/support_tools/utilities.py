a427d28a6cf0e4d6afc65664645f764708fc7423
TariqAHassan <tariqaahassan@gmail.com>
2017-03-12 12:50:53 -0300

    Refactor

--- biovida/support_tools/utilities.py
+++ biovida/support_tools/utilities.py
@@ -9,8 +9,10 @@ import os
 import shutil
 import numpy as np
 from scipy.ndimage import imread
+from os.path import join as os_join
 
 from biovida.support_tools.support_tools import isclose
+from biovida.support_tools.support_tools import is_numeric
 from biovida.support_tools.support_tools import natural_key
 from biovida.support_tools.support_tools import create_dir_if_needed
 from biovida.support_tools.support_tools import list_to_bulletpoints
@@ -34,11 +36,7 @@ def _subdirectories_in_path(path, to_block):
     :return: see description.
     :rtype: ``list``
     """
-    to_return = list()
-    for i in os.listdir(path):
-        if os.path.isdir(os.path.join(path, i)) and i not in to_block:
-            to_return.append(os.path.join(path, i))
-    return to_return
+    return [os_join(path, i) for i in os.listdir(path) if os.path.isdir(os_join(path, i)) and i not in to_block]
 
 
 def _train_val_test_error_checking(data_dir, target_dir, action, delete_source, existing_files, tvt):
@@ -60,7 +58,7 @@ def _train_val_test_error_checking(data_dir, target_dir, action, delete_source, 
     :type tvt: ``dict``
     """
     if not tvt:
-        raise ValueError("None of `train`, `validation` and `test` are numeric.")
+        raise ValueError("At least one of `train`, `validation` and `test` must be numeric.")
     for k, v in tvt.items():
         if v is True:
             raise ValueError("`{0}` cannot be `True`".format(k))
@@ -78,11 +76,9 @@ def _train_val_test_error_checking(data_dir, target_dir, action, delete_source, 
         if len(v) < min_number_of_files:
             raise InsufficientNumberOfFiles("\nThe '{0}' subdirectory in '{1}'\nonly contains {2} files, "
                                             "which is too few to distribute over {3} target locations.\n"
-                                            "Calculation: "
-                                            "len([{4}]) * len([{5}]) = {3}.".format(k, data_dir, len(v),
-                                                                                    min_number_of_files,
-                                                                                    ", ".join(tvt.keys()),
-                                                                                    ", ".join(existing_files.keys())))
+                                            "Calculation: len([{4}]) * len([{5}]) = {3}.".format(
+                                            k, data_dir, len(v), min_number_of_files, ", ".join(tvt.keys()),
+                                            ", ".join(existing_files.keys())))
 
 
 def _existing_files_dict_gen(directory, to_block):
@@ -97,17 +93,12 @@ def _existing_files_dict_gen(directory, to_block):
     :return: a dictionary of the form: ``{subdirectory in ``directory``: [file_path, file_path, ...], ...}.
     :rtype: ``dict``
     """
-    def list_dirs(path):
-        return [i for i in os.listdir(path) if os.path.isdir(os.path.join(path, i))]
+    directories = [i for i in os.listdir(directory) if os.path.isdir(os_join(directory, i))]
 
     def list_files(path):
-        return [os.path.join(path, i) for i in os.listdir(path) if not i.startswith('.')]
+        return [os_join(path, i) for i in os.listdir(path) if not i.startswith('.')]
 
-    existing_files = dict()
-    for d in list_dirs(directory):
-        if d not in to_block:
-            existing_files[d] = list_files(os.path.join(directory, d))
-    return existing_files
+    return {d: list_files(os_join(directory, d)) for d in directories if d not in to_block}
 
 
 def _list_divide(l, tvt):
@@ -142,11 +133,7 @@ def _list_divide(l, tvt):
     left, divided_dict = 0, dict()
     for e, (k, v) in enumerate(tvt_sorted, start=1):
         right = len(l_sorted) * v
-        if e != len(tvt.keys()):
-            right_full = int(left) + int(right)
-        else:
-            # Be greedy if on the last iteration.
-            right_full = len(l_sorted)
+        right_full = int(left) + int(right) if e != len(tvt.keys()) else len(l_sorted)
         divided_dict[k] = l_sorted[int(left):right_full]
         left += right
     return sorted(divided_dict.items(), key=lambda x: order_dict.get(x[0]))
@@ -163,6 +150,36 @@ def _output_dict_with_ndarrays(dictionary):
     :rtype: ``dict``
     """
     return {k: {k2: np.array([imread(i) for i in v2]) for k2, v2 in v.items()} for k, v in dictionary.items()}
+
+
+def _train_val_test_engine(action, tvt, existing_files, target_path):
+    """
+
+    Engine to power ``train_val_test()``.
+
+    :param action: as evolved in side ``train_val_test()``.
+    :type action: ``str``
+    :param tvt: as evolved in side ``train_val_test()``.
+    :type tvt: ``dict``
+    :param existing_files: as evolved in side ``train_val_test()``.
+    :type existing_files: ``dict``
+    :param target_path: as evolved in side ``train_val_test()``.
+    :type target_path: ``str``
+    :return: a nested dictionary of the form ``{'train'/'val'/'test': {existing_files.key: [file, file, ...], ...}, ...}``.
+    :rtype: ``dict``
+    """
+    output_dict = dict()
+    for k, v in existing_files.items():
+        for k2, v2 in _list_divide(v, tvt):
+            if k2 not in output_dict:
+                output_dict[k2] = {k: v2}
+            else:
+                output_dict[k2][k] = v2
+            if action == 'copy':
+                target = create_dir_if_needed(directory=os_join(target_path, os_join(k2, k)))
+                for i in v2:
+                    shutil.copy2(i, os_join(target, os.path.basename(i)))
+    return output_dict
 
 
 def train_val_test(data_dir,
@@ -318,7 +335,7 @@ def train_val_test(data_dir,
     existing_dirs = _subdirectories_in_path(data_dir, to_block=groups)
 
     # Extract those of the train, validation, test (tvt) params which are numeric.
-    tvt = {k: v for k, v in locals().items() if k in groups and isinstance(v, (float, int)) and not isinstance(v, bool)}
+    tvt = {k: v for k, v in locals().items() if k in groups and is_numeric(v)}
 
     # Generate a dictionary of files in `data_dir`
     existing_files = _existing_files_dict_gen(directory=data_dir, to_block=groups)
@@ -327,17 +344,8 @@ def train_val_test(data_dir,
     _train_val_test_error_checking(data_dir=data_dir, target_dir=target_dir, action=action,
                                    delete_source=delete_source, existing_files=existing_files, tvt=tvt)
 
-    output_dict = dict()
-    for k, v in existing_files.items():
-        for k2, v2 in _list_divide(v, tvt):
-            if k2 not in output_dict:
-                output_dict[k2] = {k: v2}
-            else:
-                output_dict[k2][k] = v2
-            if action == 'copy':
-                target = create_dir_if_needed(directory=os.path.join(target_path, os.path.join(k2, k)))
-                for i in v2:
-                    shutil.copy2(i, os.path.join(target, os.path.basename(i)))
+    output_dict = _train_val_test_engine(action=action, tvt=tvt,
+                                         existing_files=existing_files, target_path=target_path)
 
     if verbose:
         print("\nStructure:\n")
