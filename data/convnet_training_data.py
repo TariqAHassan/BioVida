ca91dbac62650d7a7cdbb98cacb81c00c4132d5f
TariqAHassan <tariqaahassan@gmail.com>
2017-03-23 21:17:44 -0300

    Add Valid Images and Refactor

--- data/convnet_training_data.py
+++ data/convnet_training_data.py
@@ -7,38 +7,36 @@
 # Imports
 import os
 from os.path import join as os_join
+from biovida.images import OpeniInterface
 from biovida.images import ImageProcessing
 from biovida.images.image_cache_mgmt import image_divvy
-
-from data.source_data.training_images_from_openi import opi
+from biovida.support_tools.utilities import train_val_test
 
 from data.synthesized_data.support_tools import base_images
 from data.synthesized_data.text import occluding_text_creator
 from data.synthesized_data.arrows import arrows, arrow_creator
+from data.synthesized_data.valid import valid_image_creator
 from data.synthesized_data.grids import grid_creator
 
-
 from data._private.paths import output_dir
+from data._private.paths import target_dir
 
 
 # ----------------------------------------------------------------------------------------------------------
 # Source Data
 # ----------------------------------------------------------------------------------------------------------
 
-
-ip = ImageProcessing(opi, db_to_extract='cache_records_db')
-ip.grayscale_analysis()
-
+opi = OpeniInterface()
 
 def divvy_rule(row):
-    if row['grayscale'] == True:
-        if 'grids' == row['image_problems_from_text'][0]:
-            return os_join(output_dir, 'grids')
-        elif 'arrows' == row['image_problems_from_text'][0]:
-            return os_join(output_dir, 'arrows')
+    if 'grids' == row['image_problems_from_text'][0]:
+        return os_join(output_dir, 'grids')
+    elif 'arrows' == row['image_problems_from_text'][0]:
+        return os_join(output_dir, 'arrows')
 
 
-divvy_info = image_divvy(ip, divvy_rule=divvy_rule, action='copy')
+divvy_info = image_divvy(opi, db_to_extract='cache_records_db',
+                         divvy_rule=divvy_rule, action='copy')
 
 
 # ----------------------------------------------------------------------------------------------------------
@@ -54,11 +52,21 @@ def number_of_images_in_dir(path):
 
 
 # -----------------------------------------
+# Valid
+# -----------------------------------------
+
+
+# os.makedirs(os_join(output_dir, "valid_image"))
+valid_image_creator(base_images, start=0, end=TOTAL_PER_GROUP,
+                    general_name="valid_image", save_location=os_join(output_dir, "valid_image"))
+
+
+# -----------------------------------------
 # Text
 # -----------------------------------------
 
 
-os.makedirs(os_join(output_dir, "text"))
+# os.makedirs(os_join(output_dir, "text"))
 occluding_text_creator(base_images, start=0, end=TOTAL_PER_GROUP,
                        general_name="text", save_location=os_join(output_dir, "text"))
 
@@ -69,7 +77,7 @@ occluding_text_creator(base_images, start=0, end=TOTAL_PER_GROUP,
 
 
 arrows_end = TOTAL_PER_GROUP - number_of_images_in_dir(os_join(output_dir, 'arrows'))
-# 16686
+# 17287
 
 if arrows_end > 0:
     arrow_creator(base_images, arrows, start=0, end=arrows_end,
@@ -82,20 +90,20 @@ if arrows_end > 0:
 
 
 grids_end = TOTAL_PER_GROUP - number_of_images_in_dir(os_join(output_dir, 'grids'))
-# 12737
+# 12905
 
 if grids_end > 0:
     grid_creator(base_images, start=0, end=grids_end,
                  general_name='grid', save_location=os_join(output_dir, 'grids'))
 
 
+# ----------------------------------------------------------------------------------------------------------
+# Split into Train/Val/Test
+# ----------------------------------------------------------------------------------------------------------
 
 
-
-
-
-
-
+output = train_val_test(data=output_dir, train=0.75, validation=0.15, test=0.1,
+                        action='copy', target_dir=target_dir)
 
 
 
