b83fa378a6d5832a37d2c68b6dd0249bce991aa0
TariqAHassan <tariqaahassan@gmail.com>
2017-03-28 03:45:17 -0300

    Update Imports

--- biovida/support_tools/__init__.py
+++ biovida/support_tools/__init__.py
@@ -2,3 +2,4 @@
 from biovida.support_tools.printing import dict_pprint
 from biovida.support_tools.printing import pandas_pprint
 from biovida.support_tools.utilities import train_val_test
+from biovida.support_tools.utilities import reverse_train_val_test
