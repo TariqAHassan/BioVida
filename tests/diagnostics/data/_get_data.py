12d3790f5ac747af99a6672842156a9ec9d14c50
TariqAHassan <tariqaahassan@gmail.com>
2017-04-09 17:21:50 -0300

    Refactor

--- tests/diagnostics/data/_get_data.py
+++ tests/diagnostics/data/_get_data.py
@@ -8,7 +8,7 @@ import requests
 
 
 # ----------------------------------------------------------------------------------------------------------
-# DiseaseOntInterface - Disease Ontology
+# DiseaseOntInterface
 # ----------------------------------------------------------------------------------------------------------
 
 
@@ -21,38 +21,21 @@ with open("tests/diagnostics/data/obo_file.txt", "w") as file:
 
 
 # ----------------------------------------------------------------------------------------------------------
-# DiseaseSymptomsInterface - Disease Ontology
+# DiseaseSymptomsInterface
 # ----------------------------------------------------------------------------------------------------------
 
 
+hsdn_url = "https://raw.githubusercontent.com/LABrueggs/HSDN/master/Combined-Output.tsv"
+hsdn_file = requests.get(hsdn_url, stream=True).text
 
 
+with open("tests/diagnostics/data/hsdn.tsv", "w") as file:
+    file.write(hsdn_file)
 
 
+rephetio_ml_url = "https://github.com/dhimmel/medline/raw/0c9e2905ccf8aae00af5217255826fe46cba3d30/data/disease-symptom-cooccurrence.tsv"
+rephetio_ml_file = requests.get(rephetio_ml_url, stream=True).text
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+with open("tests/diagnostics/data/rephetio_ml.tsv", "w") as file:
+    file.write(rephetio_ml_file)
