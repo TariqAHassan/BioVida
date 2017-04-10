0fca2e174714f1efb5fb26661291271f88949988
TariqAHassan <tariqaahassan@gmail.com>
2017-04-10 03:15:16 -0300

    Update Supported Versions

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     classifiers=['Development Status :: 3 - Alpha',
                  'Natural Language :: English',
                  'Intended Audience :: Science/Research',
-                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.4',
                  'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'License :: OSI Approved :: BSD License'
